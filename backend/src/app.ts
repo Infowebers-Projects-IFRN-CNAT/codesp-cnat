ed52222a80c8e7fc2e2f527abf83f77e2f11e44a
Arthur Melo <53615432+arthurmelo777@users.noreply.github.com>
2024-01-17 14:37:31 +0000

    Inicializa o Express com typescript

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import { Router, Request, Response } from 'express'
+
+const app = express()
+const route = Router()
+
+app.use(express.json())
+
+route.get('/', (req: Request, res: Response) => {
+    res.json({"message": 'teste'})
+})
+
+app.use(route)
+
+app.listen(3333, ()=> 'servidor rodando em 3333')
