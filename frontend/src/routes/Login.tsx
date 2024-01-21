19dcb08b70a26e80d8775e6d9aa2b35a6c9fc0b0
Caca404 <christia10k@hotmail.com>
2024-01-20 22:23:48 -0300

    :lipstick: style: done login page with animations, responsive version and background slide

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import { useEffect, useState } from 'react';
 import '../css/_login.css';
 
 const Login = () => {
+
     return (
         <>
             <section>
@@ -9,8 +10,9 @@ const Login = () => {
                     <h1 data-aos="fade-down" className="text-center mb-1">
                         Bem vindo ao CODESP
                         </h1>
-                    <div className="d-flex justify-content-between align-items-center g-2">
-                        <div className="form-container" data-aos="fade-right" data-aos-delay="500">
+                    <div className="d-flex flex-column flex-lg-row justify-content-between align-items-center gap-2">
+                        <div className="images-pseudo position-relative d-block d-lg-none"></div>
+                        <div className="form-container position-relative z-2" data-aos="fade-right" data-aos-delay="500">
                             <h4 className="text-center subtitle mb-4">
                                 Faça login ou <a href="#" className="text-decoration-none">Registre-se</a> no nosso sistema.
                             </h4>
@@ -28,9 +30,9 @@ const Login = () => {
                                 </div>
                             </form>
                         </div>
-                        <div className="image" data-aos="fade-left" data-aos-delay="500">
+                        <div className="image position-relative" data-aos="fade-left" data-aos-delay="500">
                             <img src="./src/assets/balls.svg" alt="Icones de bolas" 
-                                className="w-100"/>
+                                className="w-100 d-none d-lg-block"/>
                         </div>
                     </div>
                 </div>
